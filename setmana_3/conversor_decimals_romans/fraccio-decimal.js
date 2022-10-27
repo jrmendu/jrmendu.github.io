b29b7ded4d6c1b82ee8b32c50aa15539562e0716
jr <jrmendu@gmail.com>
2022-10-27 13:09:04 +0200

    Conversor Decimals a Fraccions

--- setmana_3/conversor_decimals_romans/fraccio-decimal.js
+++ setmana_3/conversor_decimals_romans/fraccio-decimal.js
@@ -6,9 +6,22 @@ function convertir() {
     let decimales = document.getElementById("decimales").options.selectedIndex;
     let resultado = (numerador / denominador);
 
-    if (denominador == 0) {
-
+    if (denominador === 0) {
+        alert("El denominador no pot ser 0");
+        return "";
     }
     document.getElementById("resultados").innerText = String(resultado.toFixed(decimales));
+}
+
+function reset() {
+    let numerador = document.getElementById("myInputA")
+    let denominador = document.getElementById("myInputB")
+    let resultat = document.getElementById("resultados")
+    let decimales = document.getElementById("decimales")
+
+    numerador.value = "";
+    denominador.value = "";
+    resultat.innerText = "";
+    decimales.options['selectedIndex'] = 2;
 }
 
