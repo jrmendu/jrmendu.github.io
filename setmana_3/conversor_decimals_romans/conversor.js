edde8fb422ef80fac34b4384ddc734931a03f214
jr <jrmendu@gmail.com>
2022-10-27 09:26:45 +0200

    Estils CSS a calculadora

--- setmana_3/conversor_decimals_romans/conversor.js
+++ setmana_3/conversor_decimals_romans/conversor.js
@@ -31,6 +31,46 @@ function convertir() {
     // ordenar i eliminar els simbols necesaris
 }
 
+// Fn que procesa el array del número 
+// Recibe array de descomponer(num) -> [1, 9, 8, 9]
+// Devuelve string de caracteres
+function procesarArray(arr) {
+    let char0, char1, char2, char3;
+    let str;
+
+    char0 = String(arr[0]); // miles -> 0, 1, 2, 3
+    char1 = String(arr[1]); // centenas
+    char2 = String(arr[2]); // decenas
+    char3 = String(arr[3]); // unidades
+
+    // -- Repetir para cada char --
+    // Si es cero -> nada -> ""
+    
+    // Miles: 0, 1, 2, 3
+    if( char0 == 0 ){ char0 = "" }
+    else if( char0 == 1 ){ char0 = simboloRomano(1000) }
+    else if( char0 == 2 ){ char0 = simboloRomano(1000).repeat(2) }
+    else if( char0 == 3 ){ char0 = simboloRomano(1000).repeat(3) }
+
+    // Centenas: 0, 1, ..., 9
+    if( char1 == 0 ){ char1 = "" }
+    else if( char1 == 1 ){ char1 = simboloRomano(1000) }
+    else if( char1 == 2 ){ char1 = simboloRomano(1000).repeat(2) }
+    else if( char1 == 3 ){ char1 = simboloRomano(1000).repeat(3) }
+    // Decenas: 0, 1 ... 9
+
+    // Unidades: 0, ... 9
+
+    // Si es 1, 5, 10, 50, 100, 500, 1000 -> simboloRomano(num)
+    simbolosValidos = [1, 5, 10, 50, 100, 500, 1000]
+    
+    // Si es cualquier otro valor, ej. 4, 9... 
+        // -> lógica para pedir 5 - 1 -> IV o 10 - 1 -> IX
+    
+    str = char0 + char1 + char2 + char3; 
+    return str;
+}
+
 // convertir els numeros en lletres
 function simbolosRomano(num) {
         /* 
@@ -52,6 +92,8 @@ function simbolosRomano(num) {
     else if (num === 1000)  {return "M";} 
     else                    {return NaN;}
 }
+
+
 
 /* Descomposició dels numeros en Centenes, Desenes, Unitats
     1989    --> [1,9,8,9]   --> [M,C,D,U]   --> [M,C,X,I]    */
