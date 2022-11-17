217d92b49abbfa4f37d9da60510e8ae0a6f2ece6
jr <jrmendu@gmail.com>
2022-11-17 09:33:02 +0100

    Intro a les bases de dades

---
+++ setmana_2/Bases de dades/16-mañana/Products.sql
@@ -0,0 +1,8 @@
+CREATE TABLE `poo2022`.`Products` (
+  `ProductId` INT NOT NULL AUTO_INCREMENT,
+  `ProductName` VARCHAR(100) NOT NULL,
+  `SupplierId` INT NOT NULL,
+  `CategoryId` INT NOT NULL,
+  `Unit` VARCHAR(100) NOT NULL,
+  `Price` DECIMAL(6,2) NOT NULL,
+  PRIMARY KEY (`ProductId`));
---
+++ setmana_2/Bases de dades/16-mañana/SeedCustomers.sql
@@ -0,0 +1,11 @@
+/* db -> poo2022 */
+/* Tabla -> Customers */
+INSERT INTO `poo2022`.`Customers` (`CustomerName`, `ContactName`, `Address`, `City`, `PostalCode`, `Country`) VALUES ('María', 'Juanito Pérez', 'Calle 123 s/n ', 'Madrid', '28001', 'España');
+INSERT INTO `poo2022`.`Customers` (`CustomerName`, `ContactName`, `Address`, `City`, `PostalCode`, `Country`) VALUES ('Pepito', 'Mario Martínez', 'Av. de la Programación 098', 'Buenos Aires', '6660', 'Argentina');
+INSERT INTO `poo2022`.`Customers` (`CustomerName`, `ContactName`, `Address`, `City`, `PostalCode`, `Country`) VALUES ('Alfonso', 'María Santiago', 'C/ De La Fuente 345', 'Toledo', '45660', 'España');
+INSERT INTO `poo2022`.`Customers` (`CustomerName`, `ContactName`, `Address`, `City`, `PostalCode`, `Country`) VALUES ('Mark', 'John Smith', '1253 Crummit Lane', 'Omaha', '68137', 'Estados Unidos');
+INSERT INTO `poo2022`.`Customers` (`CustomerName`, `ContactName`, `Address`, `City`, `PostalCode`, `Country`) VALUES ('Andrea', 'Jose Cuervo', 'CIRC JUAN PABLO II NO. 1755 Int. NO. 5', 'Puebla', '72410', 'México');
+/* Valores Duplicados */
+INSERT INTO `poo2022`.`Customers` (`CustomerName`, `ContactName`, `Address`, `City`, `PostalCode`, `Country`) VALUES ('Alfonso', 'María Santiago', 'C/ De La Fuente 345', 'Toledo', '45660', 'España');
+INSERT INTO `poo2022`.`Customers` (`CustomerName`, `ContactName`, `Address`, `City`, `PostalCode`, `Country`) VALUES ('Mark', 'John Smith', '1253 Crummit Lane', 'Omaha', '68137', 'Estados Unidos');
+INSERT INTO `poo2022`.`Customers` (`CustomerName`, `ContactName`, `Address`, `City`, `PostalCode`, `Country`) VALUES ('Andrea', 'Jose Cuervo', 'CIRC JUAN PABLO II NO. 1755 Int. NO. 5', 'Puebla', '72410', 'México');
