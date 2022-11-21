63377569c2fb59b9f2c7d1d2b1131c05d885aa8c
jr <jrmendu@gmail.com>
2022-11-21 11:00:35 +0100

    més funcions

--- Bases de dades/CREATE-TABLES/CREATE-TABLES.sql
+++ Bases de dades/CREATE-TABLES/CREATE-TABLES.sql
@@ -85,3 +85,11 @@ CREATE TABLE `poo2022`.`Suppliers` (
   `Phone` VARCHAR(20) NOT NULL DEFAULT '+1 123 456 789',
   PRIMARY KEY (`SupplierId`));
 
+/* 10.- SUPPLIERS TABLE */
+CREATE TABLE `ProductsForOrders` (
+  `ProductId` INT NOT NULL AUTO_INCREMENT,
+  `ProductName` VARCHAR(100) NOT NULL DEFAULT 'Default Product',
+  `UnitPrice` DECIMAL(6,2) NOT NULL DEFAULT 0,
+  `UnitsInStock` INT NOT NULL DEFAULT 0,
+  `UnitsOnOrder` INT NULL,
+  PRIMARY KEY (`ProductId`));
